5a2b5b9827c8a680d89ed3c490b970028b1c7388
Михаил Волков <mmvolkov-kirov@yandex.ru>
2026-05-06 11:30:44 +0300

    контрольное задание 2 практика

--- Lesson2/dialogapp/src/main/java/ru/mirea/volkovmm/dialogapp/MainActivity.kt
+++ Lesson2/dialogapp/src/main/java/ru/mirea/volkovmm/dialogapp/MainActivity.kt
@@ -1,25 +1,29 @@
 package ru.mirea.volkovmm.dialogapp
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Кнопка из примера в методичке
         val btnShowDialog = findViewById<Button>(R.id.btnShowDialog)
-
-        // Показываем диалог при нажатии
         btnShowDialog.setOnClickListener {
             val dialogFragment = MyDialogFragment()
             dialogFragment.show(supportFragmentManager, "mirea")
         }
+
+        // В самостоятельной работе нужно добавить еще 3 кнопки в layout
+        // Если ты прописал android:onClick в XML, методы ниже сработают автоматически
     }
 
-    // Вот эти методы искал диалог:
+    // Методы для обработки нажатий в базовом MyDialogFragment:
     fun onOkClicked() {
         Toast.makeText(applicationContext, "Вы выбрали кнопку \"Иду дальше\"!", Toast.LENGTH_LONG).show()
     }
@@ -30,5 +34,28 @@ class MainActivity : AppCompatActivity() {
 
     fun onNeutralClicked() {
         Toast.makeText(applicationContext, "Вы выбрали кнопку \"На паузе\"!", Toast.LENGTH_LONG).show()
+    }
+
+    // --- МЕТОДЫ ДЛЯ САМОСТОЯТЕЛЬНОЙ РАБОТЫ (Раздел 4.4) ---[cite: 1]
+
+    // Вызов диалога выбора времени[cite: 1]
+    fun onClickTimeDialog(view: View) {
+        val timeDialog = MyTimeDialogFragment()
+        timeDialog.show(supportFragmentManager, "time_picker")
+
+        // Пример использования Snackbar[cite: 1]
+        Snackbar.make(view, "Открываем выбор времени", Snackbar.LENGTH_SHORT).show()
+    }
+
+    // Вызов диалога выбора даты[cite: 1]
+    fun onClickDateDialog(view: View) {
+        val dateDialog = MyDateDialogFragment()
+        dateDialog.show(supportFragmentManager, "date_picker")
+    }
+
+    // Вызов диалога прогресса[cite: 1]
+    fun onClickProgressDialog(view: View) {
+        val progressDialog = MyProgressDialogFragment()
+        progressDialog.show(supportFragmentManager, "progress")
     }
 }
